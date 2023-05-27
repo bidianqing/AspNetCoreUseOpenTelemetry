05215025ee57a2be02fe437d83c272b4efaae2f0
毕殿卿 <bidianqing@qq.com>
2023-05-28 00:18:36 +0800

    up

---
+++ AspNetCoreUseOpenTelemetry/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreUseOpenTelemetry.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using StackExchange.Redis;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace AspNetCoreUseOpenTelemetry.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IDatabase _redis;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger,
+            IHttpClientFactory httpClientFactory,
+            ConnectionMultiplexer connectionMultiplexer,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _redis = connectionMultiplexer.GetDatabase();
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            string requestUri = "https://www.baidu.com";
+
+            var httpClient = _httpClientFactory.CreateClient();
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(requestUri);
+
+            //var connection = new MySqlConnection(_configuration.GetConnectionString("MySqlConnectionString"));
+            //var user = new User
+            //{
+            //    Name = "bidianqing"
+            //};
+            //await connection.InsertAsync(user);
+        }
+    }
+}
--- AspNetCoreUseOpenTelemetry/Program.cs
+++ AspNetCoreUseOpenTelemetry/Program.cs
@@ -1,20 +1,53 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using StackExchange.Redis;
+using System.Diagnostics;
+using System.Net.Mime;
 
-namespace AspNetCoreUseOpenTelemetry
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
+
+var connection = ConnectionMultiplexer.Connect("localhost:6379");
+builder.Services.AddSingleton(sp =>
 {
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    return connection;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// https://devblogs.microsoft.com/dotnet/opentelemetry-net-reaches-v1-0/
+// https://github.com/open-telemetry/opentelemetry-dotnet
+// https://github.com/open-telemetry/opentelemetry-dotnet-contrib
+builder.Services.AddOpenTelemetry()
+    .WithTracing(traceProviderBuilder =>
+    {
+        traceProviderBuilder.AddSource(DiagnosticsConfig.ActivitySource.Name)
+                            .ConfigureResource(resource => resource
+                            .AddService(DiagnosticsConfig.ServiceName))
+                            .AddAspNetCoreInstrumentation()
+                            .AddHttpClientInstrumentation()
+                            .AddConsoleExporter();
+
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public static class DiagnosticsConfig
+{
+    public const string ServiceName = "MyService";
+    public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
 }
